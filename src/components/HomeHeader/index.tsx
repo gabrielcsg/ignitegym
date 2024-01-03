1937b36353f8227a5debb87c7b6cb32d99ee78b9
Gabriel Castro <gabrielcssg@gmail.com>
2024-01-02 21:59:44 -0300

    feat: home header

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,33 @@
+import { TouchableOpacity } from 'react-native';
+import { HStack, Heading, Icon, Text, VStack } from 'native-base';
+
+import userPhotoDefault from '@assets/userPhotoDefault.png';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { UserPhoto } from '../UserPhoto';
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pb={5} pt={16} px={8} alignItems="center">
+      <UserPhoto
+        alt="User Image"
+        defaultSource={userPhotoDefault}
+        size={16}
+        source={{ uri: 'https://github.com/gabrielcsg.png' }}
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Gabriel
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
